18849167c8a84d29f5175caca656a2d9e4996c1e
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-27 10:20:50 +0530

    adding files.

---
+++ if....else if....else statement/js/main.js
@@ -0,0 +1,17 @@
+var num1 = prompt("Enter the num1");
+num1 = parseInt(num1);
+var num2= prompt("Enter the num2");
+num2 = parseInt(num2);
+
+if (num1 == num2)
+{
+alert("equal");
+}
+else if (num1<num2)
+{
+alert(" less than ");
+}
+else
+{
+alert( " greater than ");
+}
